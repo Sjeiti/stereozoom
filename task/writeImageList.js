8dc126288b50ad0e779611a08860859bc7aedad7
Ron Valstar <ron@ronvalstar.nl>
2023-09-12 09:55:38 +0200

    cleanup

--- task/writeImageList.js
+++
@@ -1,8 +0,0 @@
-const {promises:{readdir, writeFile}} = require('fs')
-const testFolder = './static/img/'
-
-readdir(testFolder)
-  .then(files => {
-    console.log('files',files)
-    writeFile('src/js/imageList.js', `export const imageList = ${JSON.stringify(files)}`)
-  })
